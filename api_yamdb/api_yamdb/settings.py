a739ac64e0936313a04e518a6d66b4a283246522
Михаил Брагин <mbragins@mail.ru>
2023-05-12 21:25:54 +0500

    Commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['${{ secrets.ALLOWED_HOSTS }}']
+# ALLOWED_HOSTS = ${{ secrets.ALLOWED_HOSTS }}
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
