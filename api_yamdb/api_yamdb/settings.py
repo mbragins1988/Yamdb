f6e4a14c0ddfabc65e91d3dc9af1c19f45c14d14
Михаил Брагин <mbragins@mail.ru>
2023-05-14 20:52:23 +0500

    Commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -18,7 +18,7 @@ CONFIRMATION_CODE = ''.join(
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret
-SECRET_KEY = os.getenv('SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production
 DEBUG = os.getenv('DEBUG', False)
